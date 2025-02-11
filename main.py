5ddb2934979a78a6b79394da48a93967960413f3
Luis Gear <riveroluismanuel9@gmail.com>
2025-02-11 16:08:18 -0500

    Init

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os, re
 
 from skel.routes import ROUTES
-from lib.parser import parse
-
-# Variable line format!
-VARIABLE_FORMAT = re.compile("^[A-Za-z0-9_]+=\"$")
 
 # Data that would change in the pages
 DYNAMIC_VARIABLES = [
@@ -12,33 +8,19 @@ DYNAMIC_VARIABLES = [
     "content_of_the_page"
 ]
 
-PAGES = "skel/pages/{0}.html"
-HTML = "skel/html/{0}.html"
-
-def set_build_directory():
-    if not os.path.exists("build"):
-        os.mkdir("build")
-
-parse(PAGES, "index")
-
 def compile(truncate = False):
-    set_build_directory()
-
     if truncate:
         for files in os.listdir("build"):
             os.remove(f"build/{files}")
-    
     for _file in ROUTES:
         with open(f"build/{_file}.html", 'a') as stream:
-            for row in open(HTML.format("header")):
+            for row in open("skel/html/header.html"):
                 stream.write(row)
             stream.write("\n")
-            
-            for row in open(PAGES.format(_file)):
-                stream.write(f"{row}")
+            for row in open(f"skel/pages/{_file}.html"):
+                stream.write(f"\t\t{row}")
             stream.write("\n")
-            
-            for row in open(HTML.format("footer")):
+            for row in open("skel/html/footer.html"):
                 stream.write(row)
 
-#compile(True)
+compile(True)
