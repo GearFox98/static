1a29b4aceee696e1dc66327069f6bcf207e7aa86
Luis Manuel <32717016+GearFox98@users.noreply.github.com>
2025-02-26 15:28:38 -0500

    Redesigning HTML templates

--- skel/pages/index.py
+++ skel/pages/index.py
@@ -1,12 +1,12 @@
 from stutils import media
 
 var = "It works!"
-archie = media("btw.png")
 
 def get():
     return f'''
     <div class="card">
         <h1>{var}</h1>
-        <img src={archie} />
+        <p>Now it's time to build new stuff!</p>
+        <p>Go change the code in pages. Keep it simple and stable!</p>
     </div>
     '''
