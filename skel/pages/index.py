d24b9600a9bc15f75235fe167e8a2319e26a8b22
Luis Gear <riveroluismanuel9@gmail.com>
2025-03-07 15:57:35 -0500

    Remade demo pages

--- skel/pages/index.py
+++ skel/pages/index.py
@@ -4,9 +4,15 @@ var = "It works!"
 
 def get():
     return f'''
-    <div class="card">
+    <div class="container py-5">
         <h1>{var}</h1>
-        <p>Now it's time to build new stuff!</p>
-        <p>Go change the code in pages. Keep it simple and stable!</p>
+        <div class="text-muted">
+            <p>Now it's time to build new stuff!</p>
+            <p>Go change the code in pages. Keep it simple and stable!</p>
+        </div>
+        <div class="dgrid gap-2">
+            <a class="btn btn-warning" href="https://gearfox98.github.com/static/wiki" target="_blank" rel="noopener">Documentation</a>
+            <a class="btn btn-secondary" href="about.html">About page</a>
+        </div>
     </div>
     '''
